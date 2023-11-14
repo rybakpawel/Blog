d966cc5a3ae68c1b38a3379b1f501a602d175b32
rybakpawel <rybakpawel92@gmail.com>
2023-11-14 22:36:42 +0100

    Add Not Found 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function NotFound() {
-    return (
-        <div>
-            <h2>Nie znaleziono podanej strony</h2>
-        </div>
-    );
+  return (
+    <div className="flex h-[calc(100vh_-_295px)] items-center justify-center">
+      <div className="mb-[25px] flex flex-col items-center">
+        <h1 className="text-2xl font-bold">Nie znaleziono strony!</h1>
+        <span className="mb-[50px] mt-[20px] text-lg">
+          Strona o podanym adresie nie istnieje.
+        </span>
+        <Link href="/">
+          <button className="mx-auto w-[276px] cursor-pointer rounded-md bg-primary px-[54px] py-[10px] text-white transition duration-300 hover:scale-105">
+            Wróc do strony głównej
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
