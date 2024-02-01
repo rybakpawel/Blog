af4322c2ce5a2824897fb18b1773980e87ec0e7f
rybakpawel <rybakpawel92@gmail.com>
2024-02-01 17:10:16 +0100

    Update CORS settings v2

--- next.config.js
+++ next.config.js
@@ -6,36 +6,33 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-  headers: {
-    "Access-Control-Allow-Origin": [
-      "https://rybakpawel.pl",
-      "https://www.rybakpawel.pl",
-    ],
-  },
-  // async headers() {
-  //   return [
-  //     {
-  //       // matching all API routes
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         {
-  //           key: "Access-Control-Allow-Origin",
-  //           value: "https://www.rybakpawel.pl",
-  //         }, // replace this your actual origin
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET,DELETE,PATCH,POST,PUT",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //         },
-  //       ],
-  //     },
-  //   ];
+  // headers: {
+  //   "Access-Control-Allow-Origin": [
+  //     "https://rybakpawel.pl",
+  //     "https://www.rybakpawel.pl",
+  //   ],
   // },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
