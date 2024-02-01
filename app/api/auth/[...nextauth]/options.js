7d8cdc4282b3b849dd001b92f0d33dd483675c0a
rybakpawel <rybakpawel92@gmail.com>
2024-02-01 16:19:11 +0100

    Update env variable

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -2,52 +2,52 @@
 import GoogleProvider from "next-auth/providers/google";
 
 export const options = {
-    providers: [
-      // GitHubProvider({
-      //   profile(profile) {
-      //     console.log("Profile GitHub: ", profile);
-  
-      //     let userRole = "GitHub User";
-      //     if (profile?.email === "rybakpawel92@gmail.com") {
-      //       userRole = "admin";
-      //     }
-  
-      //     return {
-      //       ...profile,
-      //       role: userRole,
-      //     };
-      //   },
-      //   clientId: process.env.GITHUB_ID,
-      //   clientSecret: process.env.GITHUB_Secret,
-      // }),
-      GoogleProvider({
-        profile(profile) {
-          console.log("Profile Google: ", profile);
-  
-          let userRole = "Google User";
-          if (profile?.email === "rybakpawel92@gmail.com") {
-            userRole = "admin";
-          }
+  providers: [
+    // GitHubProvider({
+    //   profile(profile) {
+    //     console.log("Profile GitHub: ", profile);
 
-          return {
-            ...profile,
-            id: profile.sub,
-            image: profile.picture,
-            role: userRole,
-          };
-        },
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_Secret,
-      }),
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-          if (user) token.role = user.role;
-          return token;
-        },
-        async session({ session, token }) {
-          if (session?.user) session.user.role = token.role;
-          return session;
-        },
+    //     let userRole = "GitHub User";
+    //     if (profile?.email === "rybakpawel92@gmail.com") {
+    //       userRole = "admin";
+    //     }
+
+    //     return {
+    //       ...profile,
+    //       role: userRole,
+    //     };
+    //   },
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_Secret,
+    // }),
+    GoogleProvider({
+      profile(profile) {
+        console.log("Profile Google: ", profile);
+
+        let userRole = "Google User";
+        if (profile?.email === "rybakpawel92@gmail.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          id: profile.sub,
+          image: profile.picture,
+          role: userRole,
+        };
       },
-}
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
